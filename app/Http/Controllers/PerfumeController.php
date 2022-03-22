bdfde2b182b556fecf00ce3e5dce8312bbe0866b
Fésű Gábor <fesugabor77@gmail.com>
2022-03-22 10:20:50 +0100

    Kesz

---
+++ app/Http/Controllers/PerfumeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Perfume;
+
+class PerfumeController extends Controller
+{
+    public function getPerfumes() {
+
+        $perfumes = Perfume::all();
+        
+
+        return view( "perfumes",["perfumes"=>$perfumes]);
+    }
+
+    public function newPerfume() {
+
+        return view( "new_perfume" );
+    }
+
+    public function storePerfume( Request $request ) {
+
+        $perfume = new Perfume;
+
+        $perfume->name = $request->name;
+        $perfume->type = $request->type;
+        $perfume->price = (int)$request->price;
+
+        $perfume->save();
+
+        return redirect( "new-perfume" );
+    }
+
+    public function editPerfume( $id ) {
+
+        $perfume = Perfume::find( $id );
+
+        return view( "edit_perfume", [
+            "perfume" => $perfume
+        ]);
+    }
+
+    public function updatePerfume( Request $request ) {
+        $perfume = Perfume::find( $request[ "id" ]);
+ 
+        $perfume->name = $request[ "name" ];
+        $perfume->type = $request[ "type" ];
+        $perfume->price = $request[ "price" ];
+     
+        $perfume->save();
+     
+        session()->flash( "success", "Sikeres frissítés" );
+        return redirect("/perfumes");
+    }
+
+    public function deletePerfume( $id ) {
+
+        $perfume = Perfume::find( $id );
+        $perfume->delete();
+
+        return redirect( "/perfumes" );
+    }
+}
---
+++ app/Http/Controllers/perfume.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class perfume extends Controller
+{
+    public function save(Request $request){
+        print_r( $request->all() );
+    }
+}
